d516cadd281c44200a33966aee6914716a973cdc
Nick ODell <nickodell@gmail.com>
2018-12-31 14:38:51 -0700

    Show output of failing test

--- regressions.bash
+++ regressions.bash
@@ -6,7 +6,7 @@ run() {
   echo "########################################## ASM"
   cat tests/expect_asm/$test_name
   echo "########################################## OUT"
-  cat tests/expect_asm/$test_name | ./lc3run > tests/expect_out/$test_name
+  cat tests/expect_asm/$test_name | ./lc3run | tee tests/expect_out/$test_name
   diff tests/expect_asm/$test_name tests/expect_asm/$test_name.correct
   diff tests/expect_out/$test_name tests/expect_out/$test_name.correct
 }
