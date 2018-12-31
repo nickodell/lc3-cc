f6220c29c98002f7e7e5cff74480f91fdbc6a434
Nick ODell <nickodell@gmail.com>
2018-12-31 09:17:13 -0700

    Add regression test

---
+++ regressions.bash
@@ -0,0 +1,13 @@
+# set -x
+
+run() {
+  test_name=$1
+  ./compile.py tests/$test_name.c > tests/expect_asm/$test_name
+  echo "########################################## ASM"
+  cat tests/expect_asm/$test_name
+  echo "########################################## OUT"
+  cat tests/expect_asm/$test_name | ./lc3run > tests/expect_out/$test_name
+  diff tests/expect_asm/$test_name tests/expect_asm/$test_name.correct
+  diff tests/expect_out/$test_name tests/expect_out/$test_name.correct
+}
+
---
+++ tests/and.c
@@ -0,0 +1,5 @@
+int main() {
+    int a = 0xB7AB;
+    a &= 0x3;
+    putchar(a + '0');
+}
