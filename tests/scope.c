8aeb24667dec6b825f995343603ded91504c2cd8
Nick ODell <nickodell@gmail.com>
2019-01-01 15:55:02 -0700

    Add test 'scope'

---
+++ tests/scope.c
@@ -0,0 +1,16 @@
+int main() {
+    int i = 0;
+    if(1) {
+        i = 1;
+        int b = 5;
+    }
+    int c = '0' + i;
+    putchar(c);
+    for(i = 0; i < 1; i++) {
+       int b = 10;
+    }
+    c = '0' + i;
+    putchar(c);
+}
+
+
